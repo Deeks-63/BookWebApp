ed5327ba74e87e714d2ec508997cc2d3aa06a385
Deekshant Goyal <deekshantgoyal63@gmail.com>
2023-08-26 21:41:37 +0530

    Author and Book API

--- 202307-ecolab-cs-main/cs-projects/BooksManagementApp/BooksWeb02/Startup.cs
+++ 202307-ecolab-cs-main/cs-projects/BooksManagementApp/BooksWeb02/Startup.cs
@@ -25,6 +25,8 @@ namespace BooksWeb02
             return services;
         }
 
+
+
         public static IApplicationBuilder ConfigureMiddlewares(this WebApplication app)
         {
             if (!app.Environment.IsDevelopment())
